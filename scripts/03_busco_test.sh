bfd4a9d2323afbe275be0bc0c0509eac662fd8dc
Cao_ Chunyang <ccao03@bora.sciclone.wm.edu>
2026-04-30 10:05:24 -0400

    scripts

--- scripts/03_busco_test.sh
+++ scripts/03_busco_test.sh
@@ -10,6 +10,7 @@ DATA_DIR=${HOME}/SUPERCOMPUTING_final_project/data
 OUTPUT_DIR=${HOME}/SUPERCOMPUTING_final_project/output
 OUT_DIR=${OUTPUT_DIR}/busco_results
 ASSEMBLY_DIR=${OUTPUT_DIR}/assembly
+REPORT_FILE=${OUTPUT_DIR}/busco_summary.md
 
 mkdir -p "${OUT_DIR}"
 
@@ -17,9 +18,29 @@ for i in $(cut -d ',' -f1 "${DATA_DIR}/SraRunTable.csv" | tail -n +2)
 do
 sample_fasta="${ASSEMBLY_DIR}/${i}_assembled/scaffolds.fasta"
 if [ -f "$sample_fasta" ]; then
-busco -i "${sample_fasta}" -o "${i}" -l enterobacterales_odb10 -m genome -c 8 --out_path "${OUT_DIR}"  --force
+busco -i "${sample_fasta}" -o "${i}" -l enterobacterales_odb10 -m genome -c 8 --out_path "${OUT_DIR}" --force
 else
 echo "cannot find $sample_fasta"
+fi
+done
+
+echo "| Sample ID | Complete (C) | Single (S) | Duplicated (D) | Fragmented (F) | Missing (M) |" > "${REPORT_FILE}"
+echo "| :--- | :---: | :---: | :---: | :---: | :---: |" >> "${REPORT_FILE}"
+
+# 2. 遍历 SraRunTable 里的样本名
+for i in $(cut -d ',' -f1 "${DATA_DIR}/SraRunTable.csv" | tail -n +2)
+do
+
+BUSCO_FILE=$(find "${OUT_DIR}/${i}" -name "short_summary.specific.*.txt" | head -n 1)
+if [ -f "$BUSCO_FILE" ]; then
+COMP=$(grep -o "C:[0-9.]*%" "$BUSCO_FILE" | cut -d':' -f2)
+SING=$(grep -o "S:[0-9.]*%" "$BUSCO_FILE" | cut -d':' -f2)
+DUPL=$(grep -o "D:[0-9.]*%" "$BUSCO_FILE" | cut -d':' -f2)
+FRAG=$(grep -o "F:[0-9.]*%" "$BUSCO_FILE" | cut -d':' -f2)
+MISS=$(grep -o "M:[0-9.]*%" "$BUSCO_FILE" | cut -d':' -f2)
+echo "| **${i}** | ${COMP} | ${SING} | ${DUPL} | ${FRAG} | ${MISS} |" >> "${REPORT_FILE}"
+else
+echo "| **${i}** | N/A | N/A | N/A | N/A | N/A |" >> "${REPORT_FILE}"
 fi
 done
 
